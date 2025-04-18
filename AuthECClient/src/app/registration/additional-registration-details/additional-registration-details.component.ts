44ec728ad48f1a53abac658687d538059e995734
Saurabh Rane <63310412+SaurabhRane1702@users.noreply.github.com>
2025-04-18 20:29:06 +0530

    New Component Added

---
+++ AuthECClient/src/app/registration/additional-registration-details/additional-registration-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-additional-registration-details',
+  imports: [],
+  templateUrl: './additional-registration-details.component.html',
+  styles: ``
+})
+export class AdditionalRegistrationDetailsComponent {
+
+}
